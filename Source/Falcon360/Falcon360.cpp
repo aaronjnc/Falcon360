d3d16e38028bf044577519f862a26227493a9c86
Aaron <aaronkingnc@gmail.com>
2023-03-30 00:56:08 -0400

    Initial Push

---
+++ Source/Falcon360/Falcon360.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Falcon360.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Falcon360, "Falcon360" );
---
+++ Source/Falcon360/Falcon360GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Falcon360GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FALCON360_API AFalcon360GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
